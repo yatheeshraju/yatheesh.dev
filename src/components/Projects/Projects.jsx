652058a1a26212c938d8f7bcffb6b2e67e8a2595
yatheeshraju <yatheesh.konduru@gmail.com>
2020-03-09 22:26:56 +0530

    projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './Projects.scss';   
+import projects from './projects.js';
+import Project from './../Project/Project';
 
 export class Projects extends Component {
+  
     render() {
         return (
             <div className='projects' id='projects'>
                 <h1> <a href='\#projects'># PROJECTS</a></h1>
-                <span></span>
+                <div>
+                {projects.map((project)=><Project key={project.id} project={project}/>)}
+                </div>
             </div>
         )
     }
---
+++ src/components/Projects/projects.js
@@ -0,0 +1,21 @@
+var projects=[{
+    id:1,
+    projectName:"TESTNG DOCS NEW DESIGN",
+    projectLink:"https://testng-docs.readthedocs.io/"
+},{
+    id:2,
+    projectName:"SIMPLE CLASSIFIDES",
+    projectLink:"https://github.com/yatheeshraju/lists"
+},{
+    id:3,
+    projectName:"EASY MERGE PDF",
+    projectLink:"https://easymergepdf.herokuapp.com/"
+},
+{ 
+  id:4,
+  projectName:"CODER WELLBEING",
+  projectLink:"https://github.com/yatheeshraju/coder-well-being"
+}
+];
+
+export default projects
