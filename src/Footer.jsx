e6cd1719a60a711b9972b7b114be3c361a101816
yatheesh <yatheesh.konduru@gmail.com>
2024-03-24 00:22:00 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,5 @@
 function Footer() {
-	return <div className="text-center text-gray-600 text-sm p-4 mt-8"> - © 2023 yatheesh - </div>;
+	return <div className="text-center text-gray-600 text-sm p-4 mt-8"> - © 2024 yatheesh - </div>;
 }
 
 export default Footer;
